f8f931b46a10e9e0f9b12299ec664c1f74053f49
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2020-06-09 11:56:52 -0300

    Add 404 page and some modifications to router

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
@@ -14,9 +12,8 @@ const API = 'http://localhost:3000/initalState';
 const Home = () => {
   const initialState = useInitialState(API);
   return initialState.length === 0 ? <h1>Loading...</h1> : (
-    <div className='App'>
+    <>
 
-      <Header />
       <Search />
       {initialState.myList?.length > 0 && (
         <Categories title='Mi lista'>
@@ -34,12 +31,10 @@ const Home = () => {
 
       <Categories title='Originales de Platzi Video'>
         <Carousel>
-        {initialState.originals?.map((item) => <CarouselItem key={item.id} {...item} />)}
+          {initialState.originals?.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
-
-      <Footer />
-    </div>
+    </>
   );
 };
 export default Home;
