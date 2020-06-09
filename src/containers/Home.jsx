ed4ce07a5d6e80711e1edb84b4e1dcd5454e21ac
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2020-06-09 16:34:09 -0300

    Add and Delete

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -16,7 +16,15 @@ const Home = ({ myList, trends, originals }) => {
       {myList?.length > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
-            <CarouselItem />
+          {myList.map(item => 
+            <CarouselItem
+              key={item.id}
+              {...item}
+              isList
+            />
+          )
+          }
+
           </Carousel>
         </Categories>
       )}
